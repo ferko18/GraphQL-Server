6dfc123ddcbc9a3a39941a4a367aa776cbd98fd0
Firealem Erko <firealemg@yahoo.com>
2019-12-13 17:14:01 -0500

    post fetching works

--- index.js
+++ index.js
@@ -4,18 +4,33 @@ const gql = require("graphql-tag");
 const mongoose = require("mongoose");
 require("dotenv").config();
 const { MONGODB } = require("./config.js");
+const Post = require("./models/Posts");
+
 //type definition
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 //resolvers
 const resolvers = {
   Query: {
-    sayHi: () => "hello"
+    getPosts: async () => {
+      try {
+        const posts = await Post.find();
+        return posts
+      } catch (error) {
+        throw new Error(error);
+      }
+    }
   }
 };
 
@@ -28,13 +43,14 @@ const server = new ApolloServer({ typeDefs, resolvers });
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log('MongoDB Connected');
+    console.log("MongoDB Connected");
     return server.listen({ port: 7000 });
   })
-  .then((res) => {
+  .then(res => {
     console.log(`Server running at ${res.url}`);
-  }).catch(error=>console.log(error))
+  })
+  .catch(error => console.log(error));
 //listen
-console.log(MONGODB)
+console.log(MONGODB);
 
 //console.log(process.env.MONGODB)
